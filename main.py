b011b4eb03ee6153e7887ef702ad6f468665a1b0
Drony <drony2@mail.ru>
2024-07-05 19:24:14 +0300

    Добавил смайлы

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import telebot
 import config
 
-
 bot = telebot.TeleBot(config.token)
 
 
@@ -12,7 +11,7 @@ def send_welcome(message):
 
 
 @bot.message_handler(commands=['video'])
-def start (message):
+def start(message):
     video = open('source/cotic.mp4', 'rb')
     bot.send_video(chat_id=message.chat.id, video=video)
     bot.send_message(message.chat.id, 'А теперь голос котиков?\nЕсли да нажми на /audio')
@@ -23,15 +22,35 @@ def start (message):
 def start(message):
     audio = open('source/mur.mp3', 'rb')
     bot.send_voice(chat_id=message.chat.id, voice=audio)
-    bot.send_message(message.chat.id, 'Вот и все возможности бота')
     print(message.text)
 
 
 @bot.message_handler(content_types=['text'])
 def cat_text(message):
+    chat_id = message.chat.id
     if message.text == "Да" or message.text == "да" or message.text == "lf":
-        bot.register_next_step_handler(bot.send_message(message.chat.id, 'Введи сколько(числом не более 4): '), step_Set_Price)
+        bot.register_next_step_handler(bot.send_message(chat_id, 'Введи сколько(числом не более 4): '), step_Set_Price)
         print(message.text)
+
+    match message.text:
+        case '🐈':
+            bot.send_message(chat_id, 'Эта котя лижит ножку')
+            print(message.text)
+        case "🐱":
+            bot.send_message(chat_id, 'Эта котя лижит ляпку')
+            print(message.text)
+        case "💥":
+            bot.send_message(chat_id, 'Это бум')
+            print(message.text)
+        case "🍜":
+            bot.send_message(chat_id, 'Это еда кись')
+            print(message.text)
+        case "❤️":
+            bot.send_message(chat_id, 'I love you')
+            print(message.text)
+        case _:
+            bot.send_message(chat_id, "Нет такой функции")
+            print("Херню просят")
 
 
 def step_Set_Price(message):
@@ -41,7 +60,7 @@ def step_Set_Price(message):
     for i in range(int(user_text)):
         phot = open(f"source/Cat{i + 1}.jpg", "rb")
         bot.send_photo(chat_id=cid, photo=phot)
-        print("кинул фото")
+        print("Кинул фото")
 
     bot.send_message(chat_id=cid, text="А хочешь видео?\nТогда нажми на /video")
 
